4df5971b4bdff2eed4708994305282003282d696
nkammah <nassim@etsy.com>
2013-01-23 22:08:54 +0000

    pulling etsy util functions into their own class

---
+++ phplib/Util/EtsyUtil.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'Autoload.php';
+
+class Util_Etsyutil {
+    public static function getRepoLocation($user) {
+        $location = getenv('ETSY_SRC');
+        if (!$location) {
+            if (preg_match(";development/([^/]+);", getcwd(), $matches) > 0) {
+                $location = "/home/$user/development/".$matches[1];
+            } else {
+                $location = "/home/$user/development/Etsyweb"; // Backwards compatibility
+            }
+        }
+        return $location;
+    }
+    
+    public static function getUser() {
+        $user = getenv('LDAP_USER');  // LDAP_USER will override USER
+        if (!$user) {
+            $user = getenv("USER");
+        }
+        return $user;
+    }
+}
