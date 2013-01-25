420c8e5864fa2a5b72cce16db6bdd19258ef920b
nkammah <nassim@etsy.com>
2013-01-25 21:55:50 +0000

    reverting back OptionParser to use getopt

--- Try/Util/OptionsParser.php
+++ Try/Util/OptionsParser.php
@@ -1,105 +1,109 @@
 <?php
 
-require_once 'Console/GetoptPlus.php';
-
 class Try_Util_OptionsParser {
-    private $config;
-    private $defaults;
+    public static function getOptions() {
+        global $argv;
 
-    public function __construct() {
-        $this->config = null;
-        $this->defaults = null;
-    }
-
-    public function getConfig() {
-        if (is_null($this->config)) {
-            $this->config = array(
-                'header' => array('"try" your changeset on Jenkins'),
-                'usage' => array('','[options] [subjobs]'),
-                'options' => array(
-                    array('short' => 'n', 'long' => 'diffonly', 'type' => 'noarg',
-                       'desc' => array('Create diff, but do not send to Jenkins')
-                    ),
-
-                    array('short' => 'v', 'long' => 'verbose', 'type' => 'noarg',
-                        'desc' => array('Show shell commands as they run.')
-                    ),
-
-                    array('short' => 'p', 'long' => 'patch', 'type' => 'mandatory',
-                        'desc' => array(
-                                    '/path/to/patch.diff',
-                                    'Don\'t generate diffs; use custom patch file instead'
-                                  )
-                    ),
-
-                    array('short' => 'c', 'long' => 'poll', 'type' => 'noarg',
-                        'desc' => array('Poll for job completion and print results')
-                    ),
-
-                    array('short' => 'P', 'long' => 'showprogress', 'type' => 'noarg',
-                        'desc' => array('print subtasks progressively as they complete (implies c)')
-                    ),
-
-                    array('short' => 's', 'long' => 'staged', 'type' => 'noarg',
-                        'desc' => array('use staged changes only to generate the diff')
-                    ),
-
-                    array('short' => 'C', 'long' => 'callback', 'type' => 'mandatory',
-                        'desc' => array('callback string',
-                                'Callback string to execute at the end of the try run.',
-                                'Use ${status} and ${url} as placeholders ' .
-                                'for the try build status and url',
-                                'Example: -C \'echo "**Try status : [\${status}](\${url})**"\''
-                                )
-                    ),
-                ),
-                'parameters' => array('[subjobs]',
-                                      'try subjobs to execute (without the "try-" prefix)','Example : unit-tests'),
-                'footer' => array(''),
+        $options = array(
+            'jobs' => array(),
+            'verbose' => false,
+            'diffonly' => false,
+            'patch' => null,
+            'staged' => false,
+            'showprogress' => false,
+            'callback' => null
             );
-        }
 
-        return $this->config;
-    }
+        $parameters = array(
+            'h' => 'help',
+            'v' => 'verbose',
+            'n' => 'diff-only',
+            'p:' => 'patch:',
+            'c:' => 'callback:',
+            'P' => 'show-progress',
+            's' => 'staged'
+        );
 
-    public function setConfig($config) {
-        $this->config = $config;
-    }
+        $opt = getopt(implode('', array_keys($parameters)), $parameters);
+        foreach ($opt as $k=>$v) {
+            switch($k) {
+                case 'h':
+                case 'help':
+                    self::showHelp();
+                    break;
 
-    public function getDefaults() {
-        if (is_null($this->defaults)) {
-            $this->defaults = array(
-                'verbose' => false,
-                'diffonly' =>false,
-                'patch' => null,
-                'staged' => false,
-                'showprogress' => false,
-                'callback' => null
-            );
-        }
-        return $this->defaults;
-    }
+                case 'v':
+                case 'verbose':
+                    $options['verbose'] = true;
+                    break;
 
-    public function setDefaults($defaults) {
-        $this->defaults = $defaults;
-    }
+                case 'n':
+                case 'diff-only':
+                    $options['dry-run'] = true;
+                    break;
 
-    public function getOptions() {
-        try {
-            list($options, $jobs) = Console_Getoptplus::getoptplus($this->getConfig(), 'short2long', true);
-        } catch(Console_GetoptPlus_Exception $e) {
-            echo $e->getMessage() . PHP_EOL;
-            exit(1);
-        }
+                case 'p':
+                case 'patch':
+                    $options['patch'] = $v;
+                    break;
 
-        foreach($options as $key=>$val) {
-            if ($val === '') {
-                $options[$key] = true;
+                case 'P':
+                case 'show-progress':
+                    $options['poll_for_completion'] = true;
+                    break;
+
+                case 's':
+                case 'staged':
+                    $options['staged-only'] = true;
+                    break;
+
+                case 'c':
+                case 'callback':
+                    if (is_array($v)) {
+                        print 'You can specify only 1 callback string' . PHP_EOL;
+                        exit(1);
+                    }
+                    $options['callback'] = $v;
+                    break;
             }
         }
+        # Now remove the options from argv to get the jobs
+        $pruneargv = array();
+        foreach ($opt as $option => $value) {
+          foreach ($argv as $key => $chunk) {
+            $regex = '/^'. (isset($option[1]) ? '--' : '-') . $option . '/';
+            if ($chunk == $value && $argv[$key-1][0] == '-' || preg_match($regex, $chunk)) {
+                array_push($pruneargv, $key);
+            }
+          }
+        }
+        while ($key = array_pop($pruneargv)) unset($argv[$key]);
 
-        $options = array_merge($this->getDefaults(), $options);
-        $options['jobs'] = $jobs;
+        $options['jobs'] = array_slice($argv, 1);
+
         return $options;
+    }
+
+    /**
+     * Display the help menu.
+     */
+    public static function showHelp() {
+        print <<<eof
+USAGE: try [options] [subjobs ...]
+
+OPTIONS:
+    -h --help                   Show help
+    -n --diff-only              Create diff, but do not send to Hudson
+    -v --verbose                Verbose (show shell commands as they're run)
+    -p|--patch=</path/to/diff>  Don't generate diffs; use custom patch file instead
+    -P --show-progress          Print subtasks progressively as they complete (implies c)
+    -s --staged                 Use staged changes only to generate the diff
+    -c|--callback <string>      Callback string to execute at the end of the try run.
+                                Use \${status} and \${url} as placeholders for the try build status and url
+                                Example: -C 'echo "**Try status : [\${status}](\${url})**"'
+eof
+        ;
+        print PHP_EOL . PHP_EOL;
+        exit(0);
     }
 }
