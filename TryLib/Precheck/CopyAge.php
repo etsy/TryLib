07ecf22772995141045d13dd880cc95a77c03b47
Nassim Kammah <nassim@etsy.com>
2013-02-10 12:12:54 -0500

    adding tests for check copy age

--- TryLib/Precheck/CopyAge.php
+++ TryLib/Precheck/CopyAge.php
@@ -21,12 +21,30 @@ class TryLib_Precheck_CopyAge implements TryLib_Precheck {
      * @param int $secs time delta in seconds
      * @return string human representation of time difference
      **/
-    public static function formatTimeDiff($secs) {
+    public function formatTimeDiff($secs) {
         $d = new DateTime();
         $d->add(new DateInterval('PT' . $secs . 'S'));
         $dv=$d->diff(new DateTime());
         return $dv->format("%m month, %d days, %h hours, %m minutes and %s seconds");
     }
+
+	public function getTimeDelta($since) {
+		return time() - strtotime($since);
+	}
+
+	public function getLastFetchDate($cmd_runner) {
+		$cmd = 'git log -1 --format=\'%cd\' --date=local';
+        if (!is_null($this->remote_branch)) {
+            $cmd .= ' origin/' . $this->remote_branch;
+        }
+
+        $ret = $cmd_runner->run($cmd);
+        if ($ret) {
+			return null;
+		} else {
+			return $cmd_runner->getOutput();
+		}
+	}
 
     /**
      * Check the age of the working copy and warn user if
@@ -36,25 +54,18 @@ class TryLib_Precheck_CopyAge implements TryLib_Precheck {
      * @param string $repo_path          location of the git repo
      **/
     function check($cmd_runner, $repo_path) {
-        $cmd = "git log -1 --format='%cd' --date=iso";
-        if (!is_null($this->remote_branch)) {
-            $cmd .= ' origin/' . $this->remote_branch;
-        }
-
-        $cmd_runner->run($cmd);
-        $output = $cmd_runner->getOutput();
-        if ( is_string($output)) {
-            $wc_date = strtotime($output);
-
-            $wc_age = time() - $wc_date;
+		$last_fetch = $this->getLastFetchDate($cmd_runner);
+        if ( !is_null($last_fetch)) {
+            $wc_age = $this->getTimeDelta($last_fetch);
             if ($wc_age >= $this->max_age_blocking) {
-                $msg = 'ERROR - you working copy is ' . self::formatTimeDiff($wc_age) . ' old.' . PHP_EOL;
+                $msg = 'ERROR - you working copy is ' . $this->formatTimeDiff($wc_age) . ' old.' . PHP_EOL;
                 $msg .= 'The code you want to `try` does not reflect the state of the repository' . PHP_EOL;
-                $msg .= 'Please run `git pull` and try again' . PHP_EOL . PHP_EOL;
+                $msg .= 'Please run `git pull` and try again';
                 $cmd_runner->terminate($msg);
             } elseif ($wc_age >= $this->max_age_warning) {
-                $cmd_runner->warn('Your working copy is ' . self::formatTimeDiff($wc_age) . ' old.');
-                $cmd_runner->warn('You may want to run `git fetch` to avoid merging conflicts in the try job.' . PHP_EOL);
+				$msg ='Your working copy is ' . $this->formatTimeDiff($wc_age) . ' old.' . PHP_EOL;
+				$msg .= 'You may want to run `git fetch` to avoid merging conflicts in the try job.';
+                $cmd_runner->warn($msg);
             }
         }
     }
---
+++ tests/phpunit/Precheck/CopyAgeTest.php
@@ -0,0 +1,150 @@
+<?php
+
+require_once "TryLib/Autoload.php";
+
+class CopyAgeTest extends PHPUnit_Framework_TestCase {
+
+    private $mock_cmd_runner;
+
+    function setUp() {
+        parent::setUp();
+		$this->mock_cmd_runner = $this->getMock('TryLib_CommandRunner');
+	}
+
+	function testGetLastFetchDateWithoutRemoteBranchSuccess() {
+		$last_fetch = 'Sun Feb 10 10:00:00 2013';
+
+		$script_runner = new TryLib_Precheck_CopyAge();
+        
+		$this->mock_cmd_runner->expects($this->once())
+							  ->method('run')
+							  ->with('git log -1 --format=\'%cd\' --date=local')
+							  ->will($this->returnValue(0));
+		
+
+		$this->mock_cmd_runner->expects($this->once())
+							  ->method('getOutput')
+							  ->will($this->returnValue($last_fetch));
+		
+		$actual = $script_runner->getLastFetchDate($this->mock_cmd_runner);	
+		
+		$this->assertEquals($last_fetch, $actual);			
+	}
+	
+	function testGetLastFetchDateWithRemoteBranchFailure() {
+		$last_fetch = 'Sun Feb 10 10:00:00 2013';
+
+		$script_runner = new TryLib_Precheck_CopyAge(24, 48, 'branch');
+        
+		$this->mock_cmd_runner->expects($this->once())
+							  ->method('run')
+							  ->with('git log -1 --format=\'%cd\' --date=local origin/branch')
+							  ->will($this->returnValue(1));
+
+		$this->mock_cmd_runner->expects($this->never())
+							  ->method('getOutput');
+				
+		$actual = $script_runner->getLastFetchDate($this->mock_cmd_runner);
+		$this->assertNull($actual);			
+	}
+	
+	function testWorkingCopyPastMaxBlockingAge() {
+		$last_fetch = 'Sun Feb 10 10:00:00 2013';
+		
+		$script_runner = $this->getMock(
+				'TryLib_Precheck_CopyAge',
+				array('getLastFetchDate', 'getTimeDelta', 'formatTimeDiff'),
+				array(12, 72, 'branch')
+		);
+		
+		$script_runner->expects($this->once())
+					  ->method('getLastFetchDate')
+					  ->with($this->mock_cmd_runner)
+					  ->will($this->returnValue($last_fetch));
+
+		$script_runner->expects($this->once())
+					  ->method('getTimeDelta')
+					  ->with($last_fetch)
+					  ->will($this->returnValue(100 * 60 * 60));
+		
+		$script_runner->expects($this->once())
+					  ->method('formatTimeDiff')
+					  ->with(100 * 60 * 60)
+					  ->will($this->returnValue('100 hours'));
+
+		$this->mock_cmd_runner->expects($this->once())
+							  ->method('terminate')
+							  ->with('ERROR - you working copy is 100 hours old.' . PHP_EOL
+								   . 'The code you want to `try` does not reflect the state of the repository' . PHP_EOL
+								   . 'Please run `git pull` and try again');
+		
+		$script_runner->check($this->mock_cmd_runner, 'path');
+	}
+
+	function testWorkingCopyPastMaxWarningAge() {
+		$last_fetch = 'Sun Feb 10 10:00:00 2013';
+		
+		$script_runner = $this->getMock(
+				'TryLib_Precheck_CopyAge',
+				array('getLastFetchDate', 'getTimeDelta', 'formatTimeDiff'),
+				array(12, 72, 'branch')
+		);
+		
+		$script_runner->expects($this->once())
+					  ->method('getLastFetchDate')
+					  ->with($this->mock_cmd_runner)
+					  ->will($this->returnValue($last_fetch));
+
+		$script_runner->expects($this->once())
+					  ->method('getTimeDelta')
+					  ->with($last_fetch)
+					  ->will($this->returnValue(24 * 60 * 60));
+		
+		$script_runner->expects($this->once())
+					  ->method('formatTimeDiff')
+					  ->with(24 * 60 * 60)
+					  ->will($this->returnValue('24 hours'));
+
+		$this->mock_cmd_runner->expects($this->never())
+							  ->method('terminate');
+
+		$this->mock_cmd_runner->expects($this->once())
+							  ->method('warn')
+							  ->with('Your working copy is 24 hours old.' . PHP_EOL
+								   . 'You may want to run `git fetch` to avoid merging conflicts in the try job.');
+		
+		$script_runner->check($this->mock_cmd_runner, 'path');
+	}
+
+
+	function testWorkingCopySuccess() {
+		$last_fetch = 'Sun Feb 10 10:00:00 2013';
+		
+		$script_runner = $this->getMock(
+				'TryLib_Precheck_CopyAge',
+				array('getLastFetchDate', 'getTimeDelta', 'formatTimeDiff'),
+				array(12, 72, 'branch')
+		);
+		
+		$script_runner->expects($this->once())
+					  ->method('getLastFetchDate')
+					  ->with($this->mock_cmd_runner)
+					  ->will($this->returnValue($last_fetch));
+
+		$script_runner->expects($this->once())
+					  ->method('getTimeDelta')
+					  ->with($last_fetch)
+					  ->will($this->returnValue(6 * 60 * 60));
+		
+		$script_runner->expects($this->never())
+					  ->method('formatTimeDiff');
+
+		$this->mock_cmd_runner->expects($this->never())
+							  ->method('terminate');
+
+		$this->mock_cmd_runner->expects($this->never())
+							  ->method('warn');
+		
+		$script_runner->check($this->mock_cmd_runner, 'path');
+	}
+}
