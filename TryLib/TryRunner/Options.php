0b3221075f0774768083d18e25224ac59f7d51c7
Yash Parghi <yash@etsy.com>
2014-03-31 15:44:36 +0000

    Add a TryRunner class for easy job configuration.

---
+++ TryLib/TryRunner/Options.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * A standard set of command-line options for use with TryRunner.
+ */
+final class TryLib_TryRunner_Options {
+
+    const USAGE_SPEC = "
+try [options...] [subjob] [subjob] ...
+--
+h,help             Show help
+n,diff-only        Create diff, but do not send to Hudson
+v,verbose          Verbose (show shell commands as they're run)
+p,patch=           Path to patch file to use instead of generating a diff
+s,staged           Use staged changes only to generate the diff
+w,whitelist=       Generate the patch for only the whitelisted files
+b,branch=          Remote branch to diff and try against [master]
+
+c,show-results     Show final try job results
+P,show-progress    Print subtasks progressively as they complete
+extra-param=       Extra parameters to send to jenkins - format key=value
+C,callback=        Callback string to execute at the end of the try run.
+                   Use \${status} and \${url} as placeholders for the try build status and url
+                   Example: --callback 'echo \"**Try status : [\${status}](\${url})**\"'
+
+jenkinsjob=        Master Job Name in Jenkins  [\$default_jenkins_job]
+jenkinsjobprefix=  Subjobs prefix              [\$default_jenkins_job_prefix]
+jenkinsserver=     URL to the jenkins server   [\$jenkins_server]
+wcpath=            Working Copy Path           [\$default_wc_path]
+";
+
+    private function __construct() {}  // Do not instantiate.
+
+    public static function parse(
+            $argv,
+            $jenkins_server,
+            $default_jenkins_job,
+            $default_jenkins_job_prefix = null,
+            $default_wc_path = null) {
+
+        $default_jenkins_job_prefix = $default_jenkins_job_prefix ?: $default_jenkins_job;
+        $default_wc_path = $default_wc_path ?: ".";
+
+        $formattedUsageSpec = self::USAGE_SPEC;
+        $formattedUsageSpec = str_replace("\$default_jenkins_job_prefix", $default_jenkins_job_prefix, $formattedUsageSpec);
+        $formattedUsageSpec = str_replace("\$default_jenkins_job", $default_jenkins_job, $formattedUsageSpec);
+        $formattedUsageSpec = str_replace("\$jenkins_server", $jenkins_server, $formattedUsageSpec);
+        $formattedUsageSpec = str_replace("\$default_wc_path", $default_wc_path, $formattedUsageSpec);
+
+        $parser = new TryLib_Util_PHPOptions_Options($formattedUsageSpec);
+
+        return $parser->parse($argv);
+    }
+}
