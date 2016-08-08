a882885d425f09a7f7883763436faa483864daf1
nkammah <nkammah@etsy.com>
2016-08-08 19:50:38 +0000

    Add backward compatibility for blocklisted files

---
+++ TryLib/Precheck/GitWarnOnBlacklisted.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @deprecated
+ */
+class TryLib_Precheck_GitWarnOnBlacklisted implements TryLib_Precheck {
+
+    function __construct(array $blocklist, $safelist = null, $staged = false) {
+		$warning = "[DEPRECATION] 'TryLib_Precheck_GitWarnOnBlacklisted'. ";
+		$warning .= "Please use 'TryLib_Precheck_GitWarnOnBlocklisted' instead." . PHP_EOL;
+		trigger_error($warning);
+	    $this->check = new TryLib_Precheck_GitWarnOnBlocklisted($blocklist, $safelist, $staged);
+	}
+
+    function check($cmd_runner, $repo_path, $upstream) {
+		$this->check->check($cmd_runner, $repo_path, $upstream);
+
+	}
+
+}
